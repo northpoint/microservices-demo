f7ae53bc66ee23bb16ab9f9409378ceb41dd0a87
Kevin Liao <Kevin.Liao1@ey.com>
2020-09-02 11:53:06 -0400

    test

--- deploy/kubernetes/kustomize.sh
+++ deploy/kubernetes/kustomize.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-cat > base.yaml
-# you can also use "kustomize build ." if you have it installed.
-exec kubectl kustomize
-EOF
-chmod +x ./kustomize.sh
+cat > job-createSecret.yaml
+
+sudo kustomize build .
